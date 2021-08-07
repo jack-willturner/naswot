5e209ef1d40146ca23324d0fece948d19fc49c4f
Jack Turner <jackwilliamturner@icloud.com>
2021-08-07 12:04:21 +0100

    yaml file parsing, nasbench201

--- naswot.py
+++ naswot.py
@@ -1,14 +1,18 @@
-import argparse
-
-import pandas as pd
-from tabulate import tabulate
-
-import torch
-import torch.nn as nn
-
 from gymnastics.datasets import get_data_loaders
 from gymnastics.benchmarks import get_benchmark
 from gymnastics.proxies import get_proxy
+
+import argparse
+import pandas as pd
+from tabulate import tabulate
+
+import torch 
+import torch.nn as nn
+import yaml
+
+from tqdm import tqdm
+
+import time
 
 parser = argparse.ArgumentParser(
     description="Evaluate a proxy on various NAS-Benchmarks"
@@ -29,7 +33,7 @@ parser.add_argument(
 
 parser.add_argument(
     "--experiment_config",
-    default="experiment_configs/nasbench_201.yaml",
+    default="experiment_configs/cifar_nasbench_sweep.yaml",
     type=str,
     help="Needs to contain: n_trials, n_samples, proxy",
 )
@@ -41,33 +45,45 @@ parser.add_argument(
     help="The folder in which I should store the results file(s)",
 )
 
-args = parser.parse_args()
+parser.add_argument(
+    "--save_file",
+    default=None,
+    type=str
+)
 
+args = parser.parse_args()
 results = []
 
 with open(args.experiment_config, "r") as file:
     experiment_config = yaml.safe_load(file)
-
 proxy = get_proxy(experiment_config["proxy"])
 
-for benchmark in experiment_config["benchmarks"]:
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+for benchmark_name in experiment_config["benchmarks"]:
+    print(benchmark_name + "...")
+    benchmark = experiment_config["benchmarks"][benchmark_name]
 
     search_space = get_benchmark(
         benchmark["name"], path_to_api=benchmark["path_to_api"]
     )
-
-    train_loader, _, _ = get_data_loaders(
-        benchmark["dataset"], benchmark["path_to_dataset"]
+    train_loader= get_data_loaders(
+        benchmark["dataset"], benchmark["path_to_dataset"], batch_size=128,
     )
 
-    for _ in range(experiment_config["num_trials"]):
+    for trial in range(experiment_config["num_trials"]):
+
+        print(f"Trial number {trial}...")
 
         best_score: float = 0.0
 
-        for _ in range(experiment_config["num_samples"]):
-            minibatch: torch.Tensor = train_loader.sample_minibatch()
-            model: nn.Module = search_space.sample_random_architecture()
+        for _ in tqdm(range(experiment_config["num_samples"])):
+            
+            minibatch, target = train_loader.sample_minibatch()
+            model: nn.Module = search_space.sample_random_architecture(single_output=True)
 
+            minibatch = minibatch.to(device)
+            model = model.to(device)
             score: float = proxy.score(model, minibatch)
 
             if score > best_score:
@@ -101,5 +117,5 @@ results = pd.DataFrame(
 
 print(tabulate(results, headers="keys", tablefmt="psql"))
 
-results.to_pickle(f"{args.path_to_results}/{args.experiment_config}.pd")
+results.to_pickle(f"{args.path_to_results}/{args.save_file}.pd")
 
